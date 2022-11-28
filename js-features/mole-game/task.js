0abf4d803267a16e0e8e519dc79d89b50b618ad7
Dmitriy Novokreschenov <NovokrDm@yandex.ru>
2022-11-28 19:29:39 +0300

    Task completed

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,31 +1,27 @@
 getHole = index => document.getElementById(`hole${index}`);
-const field = document.getElementById('hole-game');
 const hitСounter = document.getElementById('dead');
 const missСounter = document.getElementById('lost');
 
+// На лекции мы не касались обработчика событий. 
+// Почему в данном сучае он сработал, а onclick нет? Они же оба выполняются асинхронно?
 
-// Я не могу догадаться, как сделать, чтобы цикл выполнялся каждый раз, когда мы кликаем по полю. 
-// То, что сейчас написано, не работает, наверное из-за вложеной функции в обработчике событий.
-// Прошу натолкнуть на верную мысль 
-field.onсlick = () => {
-    for (let i = 1; i < 10; i++) {
-        let hole = getHole(i);
-        hole.onсlick = () => {
-            if (hole.classList.contains('hole_has-mole')) {
-                hitСounter.textContent = +hitСounter.textContent + 1;
-            } else {
-                missСounter.textContent = +missСounter.textContent + 1;
-            }
-        };
-        if (hitСounter >= 9) {
+for (let i = 1; i < 10; i++) {
+    let hole = getHole(i);
+    hole.addEventListener('click', () => {
+        if (hole.classList.contains('hole_has-mole')) {
+            hitСounter.textContent = +hitСounter.textContent + 1;
+        } else {
+            missСounter.textContent = +missСounter.textContent + 1;
+        }
+        if (hitСounter.textContent > 9) {
             alert('Победа');
             hitСounter.textContent = 0;
             missСounter.textContent = 0;
         }
-        if (missСounter >= 5) {
+        if (missСounter.textContent > 4) {
             alert('Поражение');
             hitСounter.textContent = 0;
             missСounter.textContent = 0;
         }
-    }
+    })
 }
