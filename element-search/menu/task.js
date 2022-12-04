d7a9190ebaf07bd7d1403048e1cf201230fe5ceb
Dmitriy Novokreschenov <NovokrDm@yandex.ru>
2022-12-04 18:09:18 +0300

    I hope the task is finished now

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,13 +2,13 @@ function menuOpening(event) {
     const menuItem = this.closest('.menu__item');
     const menuSub = menuItem.querySelector('.menu_sub');
     if (menuSub !== null) {
-        //Попробовал реализвать задание повышенной сложности "Одновременно не должно быть открыто более одного вложенного меню", у меня почему-то не всегда работает preventDefault, иногда перезагружается страница
-        if (document.querySelector('.menu_active') !== null) {
-            document.querySelector('.menu_active').remove('menu_active');
+        let activeMenu = document.querySelector('.menu_active');
+        if (activeMenu !== null && activeMenu != menuSub) {
+            activeMenu.classList.toggle('menu_active');
         }
-        menuSub.classList.toggle('menu_active');
-        event.preventDefault();
+        menuSub.classList.toggle('menu_active');  
     }
+    event.preventDefault();
 }
 
 Array.from(document.querySelectorAll('.menu__link')).forEach((item) => {
