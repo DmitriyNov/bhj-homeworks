0c9f85be4bbcc8d72698d444f07b182cbffe550d
Dmitriy Novokreschenov <NovokrDm@yandex.ru>
2022-12-04 17:16:28 +0300

    Task completed

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,13 +17,12 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
+    const word = Array.from(document.querySelectorAll('.symbol'));
+    window.addEventListener('keypress', (event) => {
+      const currentSymbol = this.currentSymbol.textContent.toUpperCase();
+      const enterSymbol = event.key.toUpperCase();
+      currentSymbol == enterSymbol ? this.success() : this.fail();
+    });
   }
 
   success() {
