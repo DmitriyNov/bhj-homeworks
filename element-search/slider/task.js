17d6fe63f61e3e4a8314c497b3fae809e76ae0c1
Dmitriy Novokreschenov <NovokrDm@yandex.ru>
2022-12-04 20:55:52 +0300

    Task has been finalized

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -3,35 +3,35 @@ const arrowNext = document.querySelector('.slider__arrow_next');
 const items = Array.from(document.querySelectorAll('.slider__item'));
 const dots = Array.from(document.querySelectorAll('.slider__dot'));
 
-function nextSlide() {
-    let index = items.findIndex(element => element.className === 'slider__item slider__item_active');
-    items[index].classList.remove('slider__item_active');
-    dots[index].classList.remove('slider__dot_active');
-    index === items.length - 1 ? index = 0 : index++;
-    items[index].classList.add('slider__item_active');
-    dots[index].classList.add('slider__dot_active');
+function switchSlide(index) {
+    let activeIndex = items.findIndex(element => element.className === 'slider__item slider__item_active');
+    items[activeIndex].classList.remove('slider__item_active');
+    dots[activeIndex].classList.remove('slider__dot_active');
+    let currentIndex;
+    if (index < 0) {
+        currentIndex = items.length - 1;
+    } else if (index > items.length - 1) {
+        currentIndex = 0;
+    } else {
+        currentIndex = index;
+    }
+    items[currentIndex].classList.add('slider__item_active');
+    dots[currentIndex].classList.add('slider__dot_active');
 }
 
-function prewSlide() {
-    let index = items.findIndex(element => element.className === 'slider__item slider__item_active');
-    items[index].classList.remove('slider__item_active');
-    dots[index].classList.remove('slider__dot_active');
-    index === 0 ? index = items.length - 1 : index--;
-    items[index].classList.add('slider__item_active');
-    dots[index].classList.add('slider__dot_active');
-}
+arrowPrev.addEventListener('click', () => {
+    let index = items.findIndex(element => element.className === 'slider__item slider__item_active') - 1;
+    switchSlide(index);
+});
 
-function dotSwitch(event) {
-    let index = dots.findIndex(element => element.className === 'slider__dot slider__dot_active');
-    items[index].classList.remove('slider__item_active');
-    dots[index].classList.remove('slider__dot_active');
-    index = dots.findIndex(element => element === event.target);
-    items[index].classList.add('slider__item_active');
-    dots[index].classList.add('slider__dot_active');
-}
+arrowNext.addEventListener('click', () => {
+    let index = items.findIndex(element => element.className === 'slider__item slider__item_active') + 1;
+    switchSlide(index);
+});
 
-arrowPrev.addEventListener('click', prewSlide);
-arrowNext.addEventListener('click', nextSlide);
 dots.forEach((element) => {
-    element.addEventListener('click', dotSwitch);
+    element.addEventListener('click', (event) => {
+        let index = dots.indexOf(event.target);
+        switchSlide(index);
+    });
 })
